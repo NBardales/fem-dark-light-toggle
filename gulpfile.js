408c8f8c98c31946e93e6337271bf6ec2815cfee
Nicole Bardales <contact@nicolebardales.com>
2021-11-14 20:21:16 -0600

    Added GH pages-gulp compatibility
    Followed https://medium.com/superhighfives/deploying-to-github-pages-with-gulp-c06efc527de8

--- gulpfile.js
+++ gulpfile.js
@@ -69,3 +69,14 @@ exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
 
 // Build Gulp Task
 exports.build = series(scssTask, jsTask);
+
+var gulp        = require('gulp');
+var deploy      = require('gulp-gh-pages');
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src("./dist/**/*")
+    .pipe(deploy())
+});
