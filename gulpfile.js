3842ea953b3324e7755400e78ef30c872eaddf5a
Nicole Bardales <contact@nicolebardales.com>
2021-11-14 20:52:06 -0600

    ..

--- gulpfile.js
+++ gulpfile.js
@@ -66,6 +66,3 @@ function watchTask() {
 // Default Gulp Task
 // this is what Gulp runs when you type "gulp" on the command line
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
-
-// Build Gulp Task
-exports.build = series(scssTask, jsTask);
